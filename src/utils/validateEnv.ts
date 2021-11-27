9adba1a0e709d37b239d6d6a5d8d2b295a7284ba
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-27 21:11:40 +0200

    post--commit

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -4,6 +4,12 @@ function validateEnv(): void {
     cleanEnv(process.env, {
         NODE_ENV: str({
             choices: ['development', 'production']
-        })
+        }),
+        MONGO_PASSWORD: str(),
+        MONGO_PATH: str(),
+        MONGO_USER: str(),
+        PORT: port({default: 3000}),
     })
 }
+
+export default validateEnv;
